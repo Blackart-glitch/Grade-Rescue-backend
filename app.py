5412eb48c5ff5e190597423771e55d50c08bdd62
Daniel Boye [Author] <boyelanrewaju@gmail.com>
2023-10-18 18:10:03 +0100

    added web hook  for cicd

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ app.register_blueprint(auth)
 # Enable debug mode
 app.debug = True
 
+# make do CI/CD pipeline to get around pythonanywhere restrictions
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    try:
+        #if the method is'nt post, return error
+        if request.method != 'POST':
+            return 'Wrong event type', 400
+        #pull from git
+        repo = git.Repo('https://github.com/Blackart-glitch/Grade-Rescue-backend.git')
+        origin = repo.remotes.origin
+        origin.pull()
+
+        #return to git as payload
+        return 'Updated PythonAnywhere successfully', 200
+    except Exception as e:
+        return f'Error: {str(e)}', 500
+
 # Home route
 @app.route('/')
 def hello_world():
