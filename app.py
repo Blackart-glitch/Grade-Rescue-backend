bc40c461be8d940766eb39569c6f79107c3dd3cb
Daniel Boye [Author] <boyelanrewaju@gmail.com>
2023-10-19 01:03:53 +0100

    ci/cd  agquick fixbug

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
+import git
 from flask import Flask, request
 from routes.auth import auth
 from config.dbconnect import mysql
-import git
 
-app = Flask(__name__)
+app = Flask(__name)
 app.register_blueprint(auth)
 
 # Enable debug mode
 app.debug = True
 
-# make do CI/CD pipeline to get around pythonanywhere restrictions
+# Make a CI/CD pipeline to get around PythonAnywhere restrictions
 @app.route('/update_server', methods=['POST'])
 def webhook():
     try:
-        #if the method is'nt post, return error
+        # If the method isn't POST, return an error
         if request.method != 'POST':
             return 'Wrong event type', 400
-        #pull from git
-        repo = git.Repo('https://github.com/Blackart-glitch/Grade-Rescue-backend.git')
-        origin = repo.remotes.origin
+
+        # Open the local Git repository
+        repo_path = '/home/Xander/Grade-Rescue-backend'  # server local repository path
+        repo = git.Repo(repo_path)
+
+        # remote URL to pull
+        origin = repo.create_remote('origin', 'https://github.com/Blackart-glitch/Grade-Rescue-backend.git')
         origin.pull()
 
-        #return to git as payload
         return 'Updated PythonAnywhere successfully', 200
     except Exception as e:
         return f'Error: {str(e)}', 500
